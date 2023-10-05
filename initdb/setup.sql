41cc3b0c5bcfc351a0db69aff3dd72c99c6f0112
MeiNW <47629641+MeiNW@users.noreply.github.com>
2023-10-05 16:52:19 +0900

    Delete initdb directory
    データベース初期化クエリ削除(from pleasanter ver 1.3.47.0)

--- initdb/setup.sql
+++
@@ -1,7 +0,0 @@
-create user "Implem.Pleasanter_Owner" with password '<Any Owner password>';
-create schema authorization "Implem.Pleasanter_Owner";
-create user "Implem.Pleasanter_User" with password '<Any User password>';
-create schema authorization "Implem.Pleasanter_User";
-create database "Implem.Pleasanter" with owner "Implem.Pleasanter_Owner";
-\c "Implem.Pleasanter";
-CREATE EXTENSION IF NOT EXISTS pg_trgm;
